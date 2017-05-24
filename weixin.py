ab1159f12233fddc8ff1300cb51f764c1d889885
zhiyong <7944485022@qq.com>
2017-05-24 23:38:44 +0800

    图灵

--- weixin.py
+++ weixin.py
@@ -8,7 +8,7 @@ from lxml import etree
 
 class WeixinInterface:
 
-    def __index__(self):
+    def __init__(self):
         self.app_root = os.path.dirname(__file__)
         self.templates_root = os.path.join(self.app_root, 'templates')
         self.render = web.template.render(self.templates_root)
