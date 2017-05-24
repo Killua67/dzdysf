6eeaab0e901895e9220f85ba4b8d7597c6f64c00
zhiyong <7944485022@qq.com>
2017-05-24 21:32:35 +0800

    wx

--- index.wsgi
+++ index.wsgi
@@ -1,3 +1,18 @@
-def application(environ, start_response):
-    start_response('200 ok', [('content-type', 'text/plain')])
-    return ['Hello, ZY!']
+# _*_ coding: utf-8 _*_
+
+import os
+import sae
+import web
+
+from weixin import WeixinInterface
+
+urls = (
+'/weixin','WeixinInterface'
+)
+
+app_root = os.path.dirname(__file__)
+templates_root = os.path.join(app_root, 'templates')
+render = web.template.render(templates_root)
+
+app = web.application(urls,globals()).wsgifunc()
+application = sae.creat_wsgi_app(app)
---
+++ weixin.py
@@ -0,0 +1,30 @@
+#!/usr/bin/eny python
+#_*_coding:utf-8_*_
+import web
+import os
+import hashlib
+import time
+
+class WeixinInterface:
+
+    def __index__(self):
+        self.app_root = os.path.dirname(__file__)
+        self.templates_root = os.path.join(self.app_root, 'templates')
+        self.render = web.template.render(self.templates_root)
+
+    def GET(self):
+        data = web.input()
+        signature = data.signature
+        timestamp = data.timestamp
+        nonce = data.nonce
+        echostr = data.echostr
+        token = "zhiyong"
+
+        l = [token, timestamp, nonce]
+        l.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, 1)
+        hashcode = sha1.hexdigest()
+
+        if hashcode == signature:
+            return echostr
