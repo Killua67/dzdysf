9a0813d11645e13a7db9cbc8561a36491bcabe97
wangzhiyong <794448502@qq.com>
2017-07-26 16:43:58 +0800

    添加图片处理

--- weixin.py
+++ weixin.py
@@ -60,7 +60,7 @@ class WeixinInterface:
         
         #图片信息
         elif msgType == 'image':
-            return self.render.reply_text(fromUser, toUser, int(time.time()),xml.tag)
+            return self.render.reply_text(fromUser, toUser, int(time.time()),etree.tostring(xml))
 
         else:
             return ''
