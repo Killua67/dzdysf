d7a2d5de60e7ab374f4b5282f6c0a10278587364
Killua67 <killua_67@163.com>
2017-09-29 16:40:43 +0800

    Update talk.py

--- talk.py
+++ talk.py
@@ -8,7 +8,7 @@ import json
 def talk(content, userId):
     url = 'http://www.tuling123.com/openapi/api';
     s = requests.session()
-    d = {"key": "653fd927e9ad4b19bbbb58ac07545d9a", "info": content, "userid": userId}
+    d = {"key": "", "info": content, "userid": userId}
     data = json.dumps(d)
     r = s.post(url, data=data)
     text = json.loads(r.text)
